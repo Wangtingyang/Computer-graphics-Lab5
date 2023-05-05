c830faf4d5ae2deb64963f9a297a2a4b70208a49
Wangtingyang <wangtingyang9@gmail.com>
2023-05-05 22:43:43 +0300

    upload LR5

---
+++ code/Algorithms.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace LR5
+{
+    struct VectorC
+    {
+        public VectorC(PointF begin, PointF end)
+        {
+            A = begin.X - end.X;
+            B = begin.Y - end.Y;
+        }
+        public float A;
+        public float B;
+    }
+    struct ForSutherlandCohen
+    {
+        public ForSutherlandCohen(Point min, Point max)
+        {
+            minPoint = min;
+            pMax = max;
+        }
+        public ForSutherlandCohen(float x_min, float y_min, float x_max, float y_max)
+        {
+            minPoint = new PointF(x_min, y_min);
+            pMax = new PointF(x_max, y_max);
+        }
+        public PointF minPoint;
+        public PointF pMax;
+    }
+    class Algorithms
+    {
+        public void SetRectangleClipper(float x_min, float y_min, float x_max, float y_max)
+        {
+            rect = new ForSutherlandCohen(x_min, y_min, x_max, y_max);
+        }
+        public string GetCode(PointF a)
+        {
+            string code = "";
+            code += (a.Y > rect.pMax.Y ? '1' : '0');
+            code += (a.Y < rect.minPoint.Y ? '1' : '0');
+            code += (a.X > rect.pMax.X ? '1' : '0');
+            code += (a.X < rect.minPoint.X ? '1' : '0');
+            return code;
+        }
+        public PointF MoveBit(PointF a, float k)
+        {
+            var code = GetCode(a);
+            if (code[2] == '1')
+            {
+                return new PointF(rect.pMax.X, a.Y + k * (rect.pMax.X - a.X));
+            }
+            if (code[3] == '1')
+            {
+                return new PointF(rect.minPoint.X, a.Y + k * (rect.minPoint.X - a.X));
+            }
+            if (code[0] == '1')
+            {
+                return new PointF(a.X + (1 / k) * (rect.pMax.Y - a.Y), rect.pMax.Y);
+            }
+            if (code[1] == '1')
+            {
+                return new PointF(a.X + (1 / k) * (rect.minPoint.Y - a.Y), rect.minPoint.Y);
+            }
+            return a;
+        }
+
+        public ForSutherlandCohen rect;
+        public List<VectorC> vectors;
+        public List<KeyValuePair<PointF, PointF>> edges;
+        public void SetPolygon(List<KeyValuePair<PointF, PointF>> list)
+        {
+            vectors = new List<VectorC>();
+            edges = new List<KeyValuePair<PointF, PointF>>(list);
+            for (int i = 0; i < list.Count; i++)
+            {
+                vectors.Add(new VectorC(list[i].Key, list[i].Value));
+            }
+
+        }
+
+        public bool SutherlandCohen(PointF a, PointF b, ref PointF p1, ref PointF p2)
+        {
+            string aCode = GetCode(a);
+            string bCode = GetCode(b);
+            var k = (a.Y - b.Y) / (a.X - b.X);
+            while (true)
+            {
+                if (aCode == "0000" && bCode == "0000") // Both points are inside
+                {
+                    p1 = a;
+                    p2 = b;
+                    return true;
+                }
+                else if ((Convert.ToInt32(aCode, 2) & Convert.ToInt32(bCode, 2)) != 0) // Both points are outside the same region
+                {
+                    return false;
+                }
+                else if (aCode != "0000") // a is outside
+                {
+                    if ((Convert.ToInt32(aCode, 2) & Convert.ToInt32("1000", 2)) == 8) // Top
+                    {
+                        a = MoveBit(a, k);
+                    }
+                    else if ((Convert.ToInt32(aCode, 2) & Convert.ToInt32("0100", 2)) == 4) // Bottom
+                    {
+                        a = MoveBit(a, k);
+                    }
+                    else if ((Convert.ToInt32(aCode, 2) & Convert.ToInt32("0010", 2)) == 2) // Right
+                    {
+                        a = MoveBit(a, k);
+                    }
+                    else if ((Convert.ToInt32(aCode, 2) & Convert.ToInt32("0001", 2)) == 1) // Left
+                    {
+                        a = MoveBit(a, k);
+                    }
+                    aCode = GetCode(a);
+                }
+                else if (bCode != "0000") // b is outside
+                {
+                    if ((Convert.ToInt32(bCode, 2) & Convert.ToInt32("1000", 2)) == 8) // Top
+                    {
+                        b = MoveBit(b, k);
+                    }
+                    else if ((Convert.ToInt32(bCode, 2) & Convert.ToInt32("0100", 2)) == 4) // Bottom
+                    {
+                        b = MoveBit(b, k);
+                    }
+                    else if ((Convert.ToInt32(bCode, 2) & Convert.ToInt32("0010", 2)) == 2) // Right
+                    {
+                        b = MoveBit(b, k);
+                    }
+                    else if ((Convert.ToInt32(bCode, 2) & Convert.ToInt32("0001", 2)) == 1) // Left
+                    {
+                        b = MoveBit(b, k);
+                    }
+                    bCode = GetCode(b);
+                }
+            }
+        }
+
+        public List<PointF> CyrusBeck(PointF p1, PointF p2, List<PointF> polygon)
+        {
+            List<PointF> intersectionPoints = new List<PointF>();
+            int n = polygon.Count;
+            List<PointF> normalVectors = new List<PointF>();
+            for (int i = 0; i < n; i++)
+            {
+                PointF p3 = polygon[i];
+                PointF p4 = polygon[(i + 1) % n];
+                float nx = p4.Y - p3.Y;
+                float ny = p3.X - p4.X;
+                normalVectors.Add(new PointF(nx, ny));
+            }
+
+            PointF d = new PointF(p2.X - p1.X, p2.Y - p1.Y);
+            List<float> dotProducts = new List<float>();
+            for (int i = 0; i < n; i++)
+            {
+                PointF p3 = polygon[i];
+                float nx = normalVectors[i].X;
+                float ny = normalVectors[i].Y;
+                float dotProduct = nx * d.X + ny * d.Y;
+                dotProducts.Add(dotProduct);
+            }
+            List<float> dotProductsP1 = new List<float>();
+            for (int i = 0; i < n; i++)
+            {
+                PointF p3 = polygon[i];
+                float nx = normalVectors[i].X;
+                float ny = normalVectors[i].Y;
+                float dx = p3.X - p1.X;
+                float dy = p3.Y - p1.Y;
+                float dotProductP1 = nx * dx + ny * dy;
+                dotProductsP1.Add(dotProductP1);
+            }
+            List<float> ts = new List<float>();
+            for (int i = 0; i < n; i++)
+            {
+                if (dotProducts[i] != 0)
+                {
+                    float t = dotProductsP1[i] / dotProducts[i];
+                    ts.Add(t);
+                }
+                else
+                {
+                    ts.Add(float.NaN);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!float.IsNaN(ts[i]) && ts[i] >= 0 && ts[i] <= 1)
+                {
+                    PointF intersectionPoint = new PointF(p1.X + ts[i] * d.X, p1.Y + ts[i] * d.Y);
+                    intersectionPoints.Add(intersectionPoint);
+                }
+            }
+
+            bool p1Inside = PointInPolygon(p1, polygon);
+            bool p2Inside = PointInPolygon(p2, polygon);
+            if (p1Inside && p2Inside)
+            {
+                intersectionPoints.Add(p1);
+                intersectionPoints.Add(p2);
+                return intersectionPoints;
+            }
+            else if (p1Inside)
+            {
+                intersectionPoints.Add(p1);
+            }
+            else if (p2Inside)
+            {
+                intersectionPoints.Add(p2);
+            }
+            return intersectionPoints;
+        }
+        bool PointInPolygon(PointF point, List<PointF> polygon)
+        {
+            int count = 0;
+            int n = polygon.Count;
+            for (int i = 0; i < n; i++)
+            {
+                PointF p1 = polygon[i];
+                PointF p2 = polygon[(i + 1) % n];
+                if ((p1.Y <= point.Y && point.Y < p2.Y || p2.Y <= point.Y && point.Y < p1.Y) &&
+                    point.X < (p2.X - p1.X) * (point.Y - p1.Y) / (p2.Y - p1.Y) + p1.X)
+                {
+                    count++;
+                }
+            }
+            return count % 2 != 0;
+        }
+
+    }
+
+} 
---
+++ code/Application.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LR5
+{
+    public partial class Application : Form
+    {
+        public Application()
+        {
+            InitializeComponent();
+            algo = new Algorithms
+            {
+                rect = new ForSutherlandCohen(new Point(-5, -2), new Point(3, 2))
+            };
+            numericUpDown1.Value = scale;
+            segmentsForSutherlandCohen = ClipperSutherlandCohen("sutherlandcohen.txt");
+            segmentsForCyrusBeck = ClipperCyrusBeck("cyrusbeck.txt");
+        }
+
+        readonly List<KeyValuePair<PointF, PointF>> segmentsForSutherlandCohen;
+        readonly List<KeyValuePair<PointF, PointF>> segmentsForCyrusBeck;
+        enum Mode
+        {
+            SutherlandCohen,
+            CyrusBeck
+        }
+        Mode curMode = Mode.SutherlandCohen;
+        private int scale = 30;
+        private int shift = 15;
+        public void AddPlot(Graphics gr, Panel panel)
+        {
+            Pen pen_digits = new Pen(Color.Black, 2);
+            var cx = panel.Width / 2;
+            var cy = panel.Height / 2;
+            int cur_x = 0, cur_y = 0;
+            Font font = new Font("Arial", scale / 4);
+            int counter = 0;
+            gr.DrawString(counter.ToString(), font, pen_digits.Brush, new PointF(cx, cy));
+            while (cx - cur_x >= 0)
+            {
+                counter++;
+                cur_x += scale;
+                PointF pr = new PointF(cx + cur_x, cy);
+                PointF pl = new PointF(cx - cur_x, cy);
+                gr.DrawString(counter.ToString(), font, pen_digits.Brush, pr);
+                gr.DrawString("-" + counter.ToString(), font, pen_digits.Brush, pl);
+            }
+            counter = 0;
+            while (cy - cur_y >= 0)
+            {
+                counter++;
+                cur_y += scale;
+                PointF pl = new PointF(cx, cy + cur_y);
+                PointF pr = new PointF(cx, cy - cur_y);
+                gr.DrawString(counter.ToString(), font, pen_digits.Brush, pr);
+                gr.DrawString("-" + counter.ToString(), font, pen_digits.Brush, pl);
+            }
+            Pen penR = new Pen(Color.Black, 2);
+            gr.DrawLine(penR, cx, 0, cx, panel.Height);
+            gr.DrawLine(penR, 0, cy, panel.Width, cy);
+            PointF[] x_vec = new PointF[] { new PointF(cx, 0), new PointF(cx - 2, 5), new PointF(cx + 2, 5) };
+            PointF[] y_vec = new PointF[] { new PointF(panel.Width - 1, cy), new PointF(panel.Width - 6, cy - 2), new PointF(panel.Width - 6, cy + 2) };
+            gr.DrawPolygon(penR, x_vec);
+            gr.DrawPolygon(penR, y_vec);
+            gr.DrawString("x", new Font("Arial", scale / 4 + 1), penR.Brush, new PointF(panel.Width - 25, cy - 20));
+            gr.DrawString("y", new Font("Arial", scale / 4 + 1), penR.Brush, new PointF(cx - 15, 0));
+
+        }
+        public void Plot(Graphics gr, Panel panel)
+        {
+            Pen pen = new Pen(Color.Gray, 1);
+            Pen pen_dash = new Pen(Color.LightGray, 1);
+            pen_dash.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            gr.DrawRectangle(pen, 0, 0, panel.Width - 1, panel.Height - 1);
+            var cx = panel.Width / 2;
+            var cy = panel.Height / 2;
+            int cur_x = 0, cur_y = 0;
+            while (cx - cur_x >= 0)
+            {
+                gr.DrawLine(pen_dash, cx - cur_x, 0, cx - cur_x, panel.Height);
+                gr.DrawLine(pen_dash, cx + cur_x, 0, cx + cur_x, panel.Height);
+                cur_x += scale;
+            }
+            while (cy - cur_y >= 0)
+            {
+                gr.DrawLine(pen_dash, 0, cy - cur_y, panel.Width, cy - cur_y);
+                gr.DrawLine(pen_dash, 0, cy + cur_y, panel.Width, cy + cur_y);
+                cur_y += scale;
+
+            }
+        }
+
+        readonly Algorithms algo;
+        public Point ToPanelCoordinates(Panel panel, PointF point)
+        {
+            var cx = panel.Width / 2;
+            var cy = panel.Height / 2;
+            return new Point((int)Math.Round(point.X * (float)scale) + cx, -(int)Math.Round(point.Y * (float)scale) + cy);
+        }
+        public void DrawRectangleClipper(Graphics gr, Panel panel)
+        {
+            Pen pen = new Pen(Color.Blue, shift / 4 + 1);
+            var a = (int)Math.Round((algo.rect.pMax.X - algo.rect.minPoint.X) * (float)scale);
+            var b = (int)Math.Round((algo.rect.pMax.Y - algo.rect.minPoint.Y) * (float)scale);
+            var cx = panel.Width / 2;
+            var cy = panel.Height / 2;
+            gr.DrawRectangle(pen, new Rectangle((int)Math.Round(algo.rect.minPoint.X * (float)scale) + cx, -(int)Math.Round(algo.rect.pMax.Y * (float)scale) + cy, a, b));
+        }
+
+        public List<KeyValuePair<PointF, PointF>> ClipperSutherlandCohen(string file)
+        {
+            List<KeyValuePair<PointF, PointF>> list = new List<KeyValuePair<PointF, PointF>>();
+            StreamReader r = new StreamReader(file);
+            int num = int.Parse(r.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                var str_ = r.ReadLine().Split();
+                list.Add(new KeyValuePair<PointF, PointF>(new PointF(float.Parse(str_[0]), float.Parse(str_[1])), new PointF(float.Parse(str_[2]), float.Parse(str_[3]))));
+            }
+            var str = r.ReadLine().Split();
+            r.Close();
+            algo.SetRectangleClipper(float.Parse(str[0]), float.Parse(str[1]), float.Parse(str[2]), float.Parse(str[3]));
+            return list;
+        }
+
+        public List<KeyValuePair<PointF, PointF>> ClipperCyrusBeck(string file)
+        {
+            List<KeyValuePair<PointF, PointF>> list = new List<KeyValuePair<PointF, PointF>>();
+            List<KeyValuePair<PointF, PointF>> polygon = new List<KeyValuePair<PointF, PointF>>();
+            StreamReader r = new StreamReader(file);
+            int num = int.Parse(r.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                var str_ = r.ReadLine().Split();
+                list.Add(new KeyValuePair<PointF, PointF>(new PointF(float.Parse(str_[0]), float.Parse(str_[1])), new PointF(float.Parse(str_[2]), float.Parse(str_[3]))));
+            }
+            num = int.Parse(r.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                var str_ = r.ReadLine().Split();
+                polygon.Add(new KeyValuePair<PointF, PointF>(new PointF(float.Parse(str_[0]), float.Parse(str_[1])), new PointF(float.Parse(str_[2]), float.Parse(str_[3]))));
+            }
+            r.Close();
+            algo.SetPolygon(polygon);
+            return list;
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            AddPlot(gr, panel1);
+            Plot(gr, panel1);
+            Pen entering = new Pen(Color.Red, shift / 4 + 1);
+            Pen outering = new Pen(Color.Black, shift / 8);
+            if (curMode == Mode.SutherlandCohen)
+            {
+                DrawRectangleClipper(gr, panel1);
+                ClipperSegmentsSutherlandCohen(gr, panel1, entering, outering);
+            }
+            else
+            {
+                DrawPolygon(gr, panel1, algo.edges);
+                ClipperSegmentsCyrusBeck(gr, panel1, entering, outering);
+            }
+        }
+
+        public void ClipperSegmentsSutherlandCohen(Graphics gr, Panel panel, Pen entering, Pen outering)
+        {
+            for (int i = 0; i < segmentsForSutherlandCohen.Count; i++)
+            {
+                gr.DrawLine(outering, ToPanelCoordinates(panel, segmentsForSutherlandCohen[i].Key), ToPanelCoordinates(panel, segmentsForSutherlandCohen[i].Value));
+                PointF p1 = new PointF(0, 0);
+                PointF p2 = new PointF(0, 0);
+                if (algo.SutherlandCohen(segmentsForSutherlandCohen[i].Key, segmentsForSutherlandCohen[i].Value, ref p1, ref p2))
+                {
+                    gr.DrawLine(entering, ToPanelCoordinates(panel, p1), ToPanelCoordinates(panel, p2));
+                }
+            }
+        }
+
+        public void ClipperSegmentsCyrusBeck(Graphics gr, Panel panel, Pen entering, Pen outering)
+        {
+            for (int i = 0; i < segmentsForCyrusBeck.Count; i++)
+            {
+                PointF a = segmentsForCyrusBeck[i].Key;
+                PointF b = segmentsForCyrusBeck[i].Value;
+                gr.DrawLine(outering, ToPanelCoordinates(panel, a), ToPanelCoordinates(panel, b));
+                int n = algo.edges.Count;
+                List<PointF> ver = new List<PointF>();
+                for (int j = 0; j < n; j++)
+                    ver.Add(algo.edges[j].Key);
+                List<PointF> p = algo.CyrusBeck(a, b, ver);
+                if (p.Count > 1)
+                    gr.DrawLine(entering, ToPanelCoordinates(panel, p[0]), ToPanelCoordinates(panel, p[^1]));
+
+            }
+        }
+        private void ClearPanel()
+        {
+            panel1.Invalidate();
+        }
+        private void Panel1_SizeChanged(object sender, EventArgs e)
+        {
+            ClearPanel();
+        }
+
+        public void DrawPolygon(Graphics gr, Panel panel, List<KeyValuePair<PointF, PointF>> list)
+        {
+            Pen pen = new Pen(Color.Blue, shift / 4 + 1);
+            for (int i = 0; i < list.Count; i++)
+            {
+                gr.DrawLine(pen, ToPanelCoordinates(panel, list[i].Key), ToPanelCoordinates(panel, list[i].Value));
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            scale = (int)numericUpDown1.Value;
+            shift = (int)Math.Floor((decimal)scale / 2);
+            panel1.Invalidate();
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                curMode = Mode.SutherlandCohen;
+            }
+            else
+            {
+                curMode = Mode.CyrusBeck;
+            }
+            panel1.Invalidate();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+            panel1.Width = Width - 220;
+            panel1.Height = Height - 65;
+            panel1.Location = new Point(12, 12);
+        }
+
+        private void Application_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
